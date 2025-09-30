5343c921cb1dc47a604346a466d230b7374d565f
hfkhadijanoreen <hfkhadijanoreen@gmail.com>
2025-10-01 00:00:30 +0500

    Create src/App.svelte file

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  let name = 'world';
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>Welcome to your Svelte app.</p>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+  h1 {
+    color: #ff3e00;
+    font-size: 2em;
+    margin: 0 0 1em 0;
+  }
+</style>
